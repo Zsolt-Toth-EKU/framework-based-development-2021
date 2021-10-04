efc9d78eb80bba9784c86cdcdb90596edf8ef7c7
Zsolt Toth <tothzs87@gmail.com>
2021-10-04 17:29:43 +0200

    feat(dependency-inejction-example): initial commit

---
+++ dependency-injection-example/src/main/java/hu/ekcu/inf/framework/App.java
@@ -0,0 +1,13 @@
+package hu.ekcu.inf.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
