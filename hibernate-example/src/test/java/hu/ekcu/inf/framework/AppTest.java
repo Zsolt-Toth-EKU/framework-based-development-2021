4836eb44b77d87922897c5dfc320ec5d8717828f
Zsolt Toth <tothzs87@gmail.com>
2021-10-11 08:40:17 +0200

    feat(hibernate-example): add SpringBoot

--- hibernate-example/src/test/java/hu/ekcu/inf/framework/AppTest.java
+++ hibernate-example/src/test/java/hu/ekcu/inf/framework/AppTest.java
@@ -1,20 +1,5 @@
 package hu.ekcu.inf.framework;
 
-import static org.junit.Assert.assertTrue;
-
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
-public class AppTest 
+public class AppTest
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
 }
