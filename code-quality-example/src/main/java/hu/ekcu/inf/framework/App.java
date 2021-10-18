c1b82ef0b6f06a9fbbac9a67997d85af01943edd
Zsolt Toth <tothzs87@gmail.com>
2021-10-18 08:30:07 +0200

    chore(code-quality): add checkstyle configuration

--- code-quality-example/src/main/java/hu/ekcu/inf/framework/App.java
+++ code-quality-example/src/main/java/hu/ekcu/inf/framework/App.java
@@ -2,12 +2,9 @@ package hu.ekcu.inf.framework;
 
 /**
  * Hello world!
- *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
 }
